6a90e59dac48e7378a4e1385293dbda0da3d5ade
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2024-06-20 14:32:47 -0500

    Add files via upload

--- MATLAB/mat_to_10Xh5.m
+++ MATLAB/mat_to_10Xh5.m
@@ -11,50 +11,55 @@ if exist(filename, 'file')==2
 end
 
 if issparse(sce.X)
+    disp('sparse matrix detected...');
+else
     disp('converting to sparse...');
-    [indptr, indices, data] = convert_sparse_to_indptr(sce.X);
-
-    disp('writing matrix...');
-
-    h5create(filename, '/matrix/barcodes', size(sce.c_cell_id,1), Datatype='string');
-    h5write(filename, '/matrix/barcodes', sce.c_cell_id);
-    
-    h5create(filename, '/matrix/data', size(data,1), Datatype='uint32');
-    h5write(filename, '/matrix/data', data);
-
-    h5create(filename, '/matrix/features/name', size(sce.g,1), Datatype='string');
-    h5write(filename, '/matrix/features/name', sce.g);
-
-    h5create(filename, '/matrix/features/id', size(sce.g,1), Datatype='string');
-    h5write(filename, '/matrix/features/id', sce.g);
-
-    feature_type = string(1:size(sce.g,1));
-    feature_type(:) = 'Gene Expression';
-    h5create(filename, '/matrix/features/feature_type', size(sce.g,1), Datatype='string');
-    h5write(filename, '/matrix/features/feature_type', feature_type);
-
-    genome = string(1:size(sce.g,1));
-    genome(:) = 'scRNA';
-    h5create(filename, '/matrix/features/genome', size(sce.g,1), Datatype='string');
-    h5write(filename, '/matrix/features/genome', genome);
-
-    h5create(filename, '/matrix/indices', size(indices,1), Datatype='uint32');
-    h5write(filename, '/matrix/indices', indices);
-
-    h5create(filename, '/matrix/indptr', size(indptr,2), Datatype='uint32');
-    h5write(filename, '/matrix/indptr', indptr);
-
-    h5create(filename, '/matrix/shape', size(size(sce.X),2), Datatype='uint64');
-    h5write(filename, '/matrix/shape', size(sce.X));
-
-    barcodes = sce.c_cell_id;
-    celltypes = sce.c_cell_type_tx;
-    clusters = sce.c_cluster_id;
-    batch = sce.c_batch_id;
-    meta_filename = append(filename(1:end-3),'_meta.csv');
-    metatable = table(barcodes,celltypes,clusters,batch);
-    writetable(metatable,meta_filename);
+    sce.X = sparse(sce.X);
 end
+
+disp('converting to 10X sparse format...');
+[indptr, indices, data] = convert_sparse_to_indptr(sce.X);
+
+disp('writing matrix...');
+
+h5create(filename, '/matrix/barcodes', size(sce.c_cell_id,1), Datatype='string');
+h5write(filename, '/matrix/barcodes', sce.c_cell_id);
+
+h5create(filename, '/matrix/data', size(data,1), Datatype='uint32');
+h5write(filename, '/matrix/data', data);
+
+h5create(filename, '/matrix/features/name', size(sce.g,1), Datatype='string');
+h5write(filename, '/matrix/features/name', sce.g);
+
+h5create(filename, '/matrix/features/id', size(sce.g,1), Datatype='string');
+h5write(filename, '/matrix/features/id', sce.g);
+
+feature_type = string(1:size(sce.g,1));
+feature_type(:) = 'Gene Expression';
+h5create(filename, '/matrix/features/feature_type', size(sce.g,1), Datatype='string');
+h5write(filename, '/matrix/features/feature_type', feature_type);
+
+genome = string(1:size(sce.g,1));
+genome(:) = 'scRNA';
+h5create(filename, '/matrix/features/genome', size(sce.g,1), Datatype='string');
+h5write(filename, '/matrix/features/genome', genome);
+
+h5create(filename, '/matrix/indices', size(indices,1), Datatype='uint32');
+h5write(filename, '/matrix/indices', indices);
+
+h5create(filename, '/matrix/indptr', size(indptr,2), Datatype='uint32');
+h5write(filename, '/matrix/indptr', indptr);
+
+h5create(filename, '/matrix/shape', size(size(sce.X),2), Datatype='uint64');
+h5write(filename, '/matrix/shape', size(sce.X));
+
+barcodes = sce.c_cell_id;
+celltypes = sce.c_cell_type_tx;
+clusters = sce.c_cluster_id;
+batch = sce.c_batch_id;
+meta_filename = append('meta_',filename(1:end-3),'.csv');
+metatable = table(barcodes,celltypes,clusters,batch);
+writetable(metatable,meta_filename);
 end
 
 function [indptr, indices, data] = convert_sparse_to_indptr(X)
